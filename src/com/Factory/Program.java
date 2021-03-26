538de477547077acdd5974a55cd0f303788deb19
VitaliiVM <69699528+VitaliiVM@users.noreply.github.com>
2021-03-26 02:10:57 +0200

    Add BufferedReader

--- src/com/Factory/Program.java
+++ src/com/Factory/Program.java
@@ -1,8 +1,16 @@
 package com.Factory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Program {
-    public  static  void main(String[] args) {
-        DeveloperFactory developerFactory = createDeveloperBySpecialty("c++");
+    public static String name;
+
+    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public  static  void main(String[] args) throws IOException {
+        name = reader.readLine();
+        DeveloperFactory developerFactory = createDeveloperBySpecialty(name);
         Developer developer = developerFactory.createDeveloper();
         developer.writeCode();
     }
